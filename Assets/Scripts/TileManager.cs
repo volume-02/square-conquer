72b4a173e32c0d8824edeeb981715b09be73a645
volume-02 <borsukboris@gmail.com>
2023-09-27 22:40:53 +0700

    tile generator

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private int width = 30;
+    private int height = 30;
+
+    public GameObject camera;
+    public GameObject tilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
+
+                spawnedTile.name = $"Tile: {x}, {z}";
+
+                var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
+
+                var TileScript = spawnedTile.GetComponent<TileScript>();
+
+                TileScript.Init(isOffset);
+            }
+        }
+
+
+        camera.transform.position = new Vector3(width / 2, 25, height / 2);
+
+    }
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public Material baseColor, secondaryColor;
+
+    public void Init(bool isOffset)
+    {
+        var renderer = gameObject.GetComponent<MeshRenderer>();
+        renderer.material = isOffset ? baseColor : secondaryColor;
+    }
+}
